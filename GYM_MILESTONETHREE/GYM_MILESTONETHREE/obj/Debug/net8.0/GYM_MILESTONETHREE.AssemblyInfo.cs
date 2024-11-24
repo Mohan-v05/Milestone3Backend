62840b628475131a6a80e4fab045002165de694e
Mohanakahnan S. <UT01146@unicomtic.com>
2024-11-24 18:31:54 +0530

    24-11

--- GYM_MILESTONETHREE/GYM_MILESTONETHREE/obj/Debug/net8.0/GYM_MILESTONETHREE.AssemblyInfo.cs
+++ GYM_MILESTONETHREE/GYM_MILESTONETHREE/obj/Debug/net8.0/GYM_MILESTONETHREE.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GYM_MILESTONETHREE")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca2687ff1a5d780b85a83aeaba7dee7b729ea6a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b38aec07c117b1bab037e92fa1af6b0e3aa9c42b")]
 [assembly: System.Reflection.AssemblyProductAttribute("GYM_MILESTONETHREE")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GYM_MILESTONETHREE")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
